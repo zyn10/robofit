6b985d26f1d3169a93e2f780852ffcb29f83843e
Muhammad Zain <zainandcs@gmail.com>
2024-01-09 21:59:36 +0500

    Backend Connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-import 'package:robofit/out/output.dart';
 import 'package:robofit/out/result.dart';
-import 'package:robofit/screens/splash.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
