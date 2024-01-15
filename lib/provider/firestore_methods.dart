8c44f26436f425f94c6e806b33436e1096bbb743
Muhammad Zain <zainandcs@gmail.com>
2024-01-15 10:07:22 +0500

    Lets do it again

--- lib/provider/firestore_methods.dart
+++
@@ -1,7 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-class FireStoreMethods {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-}
